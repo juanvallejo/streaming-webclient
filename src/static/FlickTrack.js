1a469e598fb3c12c332b979bf2445b64b00a61ea
juanvallejo <jvallejo@redhat.com>
2017-06-25 17:04:47 -0400

    fix volume increase/decrease value; fix stream load not pausing
    Fixes a bug that stored a volume value as a string in localStorage;
    this prevented a valid float value from being created when adding
    a saved volume value (of type string) with an increment or decrement
    value.
    
    Also fixes a bug that attempted to pause a stream when a new stream
    was loaded, after the new stream's data had been set; resulting in
    the old stream not being actually paused by the Pause() method.

--- src/static/FlickTrack.js
+++ src/static/FlickTrack.js
@@ -913,21 +913,21 @@ var Cons = require('./constants.js');
 var Emitter = require('./proto/emitter.js');
 
 function Video(videoElement, sTrackElement) {
-	var self = this;
+    var self = this;
 
-	this.videoStreamKind = Cons.STREAM_KIND_LOCAL;
+    this.videoStreamKind = Cons.STREAM_KIND_LOCAL;
 
-	this.loadedData = null;
-	this.video = videoElement;
-	this.videoVolume = window.localStorage.volume || 0.5;
-	this.duration = null;
-	this.savedTimer = null;
-	this.sourceFileError = null;
-	this.alertShown = false;
-	this.canStartStream = false;
-	this.metadataLoaded = false;
-	this.subtitlesTrack = sTrackElement;
-	this.subtitlesTrackActivated = false;
+    this.loadedData = null;
+    this.video = videoElement;
+    this.videoVolume = window.localStorage.volume ? parseFloat(window.localStorage.volume) : 0.5;
+    this.duration = null;
+    this.savedTimer = null;
+    this.sourceFileError = null;
+    this.alertShown = false;
+    this.canStartStream = false;
+    this.metadataLoaded = false;
+    this.subtitlesTrack = sTrackElement;
+    this.subtitlesTrackActivated = false;
 
     this.ytVideoInfo = {};
     this.ytVideoCurrentTime = 0;
@@ -937,114 +937,114 @@ function Video(videoElement, sTrackElement) {
 
 
     // ignores the actual HTMLEntity when adding
-	// an event listener to this wrapper object.
-	this.EVT_IGNORE_ELEM = true;
+    // an event listener to this wrapper object.
+    this.EVT_IGNORE_ELEM = true;
 
-	// handlers
-	this.defaultSubtitlesHandler = function(path, handler) {
+    // handlers
+    this.defaultSubtitlesHandler = function(path, handler) {
         if (self.loadedData.kind !== Cons.STREAM_KIND_LOCAL) {
-        	handler('This type of stream does not support adding subtitles.');
-        	return false;
+            handler('This type of stream does not support adding subtitles.');
+            return false;
         }
 
-		if (!handler || typeof handler !== 'function') {
-			handler = function () {};
-		}
-		
-		if (!this.metadataLoaded) {
-			handler('Video metadata has not yet loaded, try again later.');
-			return false;
-		}
+        if (!handler || typeof handler !== 'function') {
+            handler = function () {};
+        }
 
-		this.subtitlesTrack.src = path;
-		this.subtitlesTrack.mode = "showing";
-		this.video.textTracks[0].mode = "showing";
-		this.subtitlesTrackActivated = true;
-		
-	};
+        if (!this.metadataLoaded) {
+            handler('Video metadata has not yet loaded, try again later.');
+            return false;
+        }
 
-	this.subtitlesHandler = this.defaultSubtitlesHandler;
+        this.subtitlesTrack.src = path;
+        this.subtitlesTrack.mode = "showing";
+        this.video.textTracks[0].mode = "showing";
+        this.subtitlesTrackActivated = true;
 
-	this.on = function(e, fn, ignore_elem) {
-		if (!this.callbacks[e]) {
-			this.callbacks[e] = [];
+    };
 
-			if(!ignore_elem) {
-				this.video.addEventListener(e, (function(e) {
-					return function(args) {
-						self.emit(e, arguments);
-					}
-				})(e));
-			}
-		}
-		this.callbacks[e].push(fn);
-	};
+    this.subtitlesHandler = this.defaultSubtitlesHandler;
 
-	this.hidePlayer = function() {
-		this.video.style.display = 'none';
-	};
+    this.on = function(e, fn, ignore_elem) {
+        if (!this.callbacks[e]) {
+            this.callbacks[e] = [];
 
-	this.showPlayer = function() {
-		this.video.style.display = 'block';
-	};
+            if(!ignore_elem) {
+                this.video.addEventListener(e, (function(e) {
+                    return function(args) {
+                        self.emit(e, arguments);
+                    }
+                })(e));
+            }
+        }
+        this.callbacks[e].push(fn);
+    };
 
-	this.hideYtPlayer = function() {
-		self.onYtPlayerReady(function() {
+    this.hidePlayer = function() {
+        this.video.style.display = 'none';
+    };
+
+    this.showPlayer = function() {
+        this.video.style.display = 'block';
+    };
+
+    this.hideYtPlayer = function() {
+        self.onYtPlayerReady(function() {
             self.ytElem.style.display = 'none';
-		});
-	};
+        });
+    };
 
-	this.showYtPlayer = function() {
-		self.onYtPlayerReady(function() {
+    this.showYtPlayer = function() {
+        self.onYtPlayerReady(function() {
             self.ytElem.style.display = 'block';
-		});
-	};
+        });
+    };
 
-	// note: uses youtube iframe-api context.
-	// called once when youtube player is ready.
-	this.callYtPlayerCallbacks = function(evt) {
-		self.ytPlayerReady = true;
-		while(self.ytReadyCallbacks.length) {
+    // note: uses youtube iframe-api context.
+    // called once when youtube player is ready.
+    this.callYtPlayerCallbacks = function(evt) {
+        self.ytPlayerReady = true;
+        while(self.ytReadyCallbacks.length) {
             self.ytReadyCallbacks.shift().call(self, self.ytPlayer.getIframe());
-		}
-	};
+        }
+    };
 
-	// "safe" wrapper for making youtube iframe api calls.
-	// Adds passed function to a callback stack that is parsed
-	// once the youtube player has loaded. If the player has already
-	// loaded, the function is then immediately called.
-	this.onYtPlayerReady = function(callback) {
-		if (this.ytPlayerReady) {
-			callback.call(self, self.ytPlayer.getIframe());
-			return;
-		}
+    // "safe" wrapper for making youtube iframe api calls.
+    // Adds passed function to a callback stack that is parsed
+    // once the youtube player has loaded. If the player has already
+    // loaded, the function is then immediately called.
+    this.onYtPlayerReady = function(callback) {
+        if (this.ytPlayerReady) {
+            callback.call(self, self.ytPlayer.getIframe());
+            return;
+        }
 
-		this.ytReadyCallbacks.push(callback);
-	};
+        this.ytReadyCallbacks.push(callback);
+    };
 
-	this.onYtPlayerStateChange = function(evt) {
+    this.onYtPlayerStateChange = function(evt) {
 
-	};
+    };
 
-	this.loadYtVideo = function(videoId) {
+    this.loadYtVideo = function(videoId) {
         self.onYtPlayerReady(function(frame) {
-			frame.contentWindow.postMessage(JSON.stringify({
-				'event': 'command',
-				'func': 'loadVideoById',
-				'args': [videoId, 0, 'large']
-			}), "*");
+            frame.contentWindow.postMessage(JSON.stringify({
+                'event': 'command',
+                'func': 'loadVideoById',
+                'args': [videoId, 0, 'large']
+            }), "*");
         });
-	};
+    };
 
-	this.seekYtVideo = function(time) {
-		self.onYtPlayerReady(function(frame) {
-			frame.contentWindow.postMessage(JSON.stringify({
-				'event': 'command',
-				'func': 'seekTo',
-				'args': [time, "true"]
-			}), "*");
+    this.seekYtVideo = function(time) {
+        self.onYtPlayerReady(function(frame) {
+            frame.contentWindow.postMessage(JSON.stringify({
+                'event': 'command',
+                'func': 'seekTo',
+                'args': [time, "true"]
+            }), "*");
         });
-	};
+    };
 
     this.setYtVideoVolume = function(vol) {
         self.onYtPlayerReady(function(frame) {
@@ -1056,7 +1056,7 @@ function Video(videoElement, sTrackElement) {
         });
     };
 
-	this.playYtVideo = function() {
+    this.playYtVideo = function() {
         self.onYtPlayerReady(function(frame) {
             frame.contentWindow.postMessage(JSON.stringify({
                 'event': 'command',
@@ -1064,7 +1064,7 @@ function Video(videoElement, sTrackElement) {
                 'args': []
             }), "*");
         });
-	};
+    };
 
     this.pauseYtVideo = function() {
         self.onYtPlayerReady(function(frame) {
@@ -1089,48 +1089,49 @@ function Video(videoElement, sTrackElement) {
                 'autoplay': 0,
                 'controls': 0,
                 'rel' : 0,
-				'cc_load_policy': 1,
-				'disablekb': 0,
-				'enablejsapi': 1,
-				'fs': 0,
-				'iv_load_policy': 3,
-				'showinfo': 0,
-				'modestbranding': 1
+                'cc_load_policy': 1,
+                'disablekb': 0,
+                'enablejsapi': 1,
+                'fs': 0,
+                'iv_load_policy': 3,
+                'showinfo': 0,
+                'modestbranding': 1
             }
         });
         this.ytElem = this.ytPlayer.getIframe();
         self.ytElem.style.display = 'none';
     };
 
-	this.init = function(location, videoElement) {
-		this.video.style.display = 'none';
-		this.video.crossorigin = "anonymous";
+    this.init = function(location, videoElement) {
+        this.video.style.display = 'none';
+        this.video.crossorigin = "anonymous";
 
-		this.subtitlesTrack.kind = "captions";
-		this.subtitlesTrack.label = "English";
-		this.subtitlesTrack.srclang = "en";
+        this.subtitlesTrack.kind = "captions";
+        this.subtitlesTrack.label = "English";
+        this.subtitlesTrack.srclang = "en";
 
-		this.video.appendChild(this.subtitlesTrack);
+        this.video.appendChild(this.subtitlesTrack);
 
-		this.subtitlesTrack.addEventListener('load', function() {
-			this.mode = "showing";
-			self.video.textTracks[0].mode = "showing";
-			self.emit('subtitlesloaded');
-		});
-	};
+        this.subtitlesTrack.addEventListener('load', function() {
+            this.mode = "showing";
+            self.video.textTracks[0].mode = "showing";
+            self.emit('subtitlesloaded');
+        });
+    };
 
-	// multi-source safe. Only handles local video streams.
-	this.appendTo = function(parent) {
-		parent.appendChild(this.video);
-	};
+    // multi-source safe. Only handles local video streams.
+    this.appendTo = function(parent) {
+        parent.appendChild(this.video);
+    };
 
-	this.load = function(data) {
-		self.loadedData = data.extra;
+    this.load = function(data) {
         self.pause();
-		self.videoStreamKind = data.extra.kind;
+        self.loadedData = data.extra;
+        self.videoStreamKind = data.extra.kind;
+        self.pause();
         if (data.extra.kind === Cons.STREAM_KIND_YOUTUBE) {
             self.hidePlayer();
-			self.showYtPlayer();
+            self.showYtPlayer();
             self.loadYtVideo(youtubeVideoIdFromUrl(data.extra.url));
             self.pause();
 
@@ -1147,35 +1148,35 @@ function Video(videoElement, sTrackElement) {
         self.hideYtPlayer();
         self.showPlayer();
 
-		self.video.src = Cons.STREAM_URL_PREFIX + data.extra.url;
-		self.video.volume = self.videoVolume;
-	};
+        self.video.src = Cons.STREAM_URL_PREFIX + data.extra.url;
+        self.video.volume = self.videoVolume;
+    };
 
-	this.play = function(time) {
-		if (!self.loadedData) {
-			console.log("WARN:", 'attempt to play video with no data loaded.');
-			return;
-		}
+    this.play = function(time) {
+        if (!self.loadedData) {
+            console.log("WARN:", 'attempt to play video with no data loaded.');
+            return;
+        }
 
-		if (self.loadedData.kind === Cons.STREAM_KIND_YOUTUBE) {
-			this.playYtVideo();
-			return;
-		}
+        if (self.loadedData.kind === Cons.STREAM_KIND_YOUTUBE) {
+            this.playYtVideo();
+            return;
+        }
 
-		if (time) {
-			this.video.currentTime = time;
-		}
-		if (this.video.muted) {
-			console.log('WARN:', 'playing muted video...');
-		}
-		try {
-			this.video.play();
-		} catch(e) {
-			console.log('EXCEPT VIDEO PLAY', e);
-		}
-	};
+        if (time) {
+            this.video.currentTime = time;
+        }
+        if (this.video.muted) {
+            console.log('WARN:', 'playing muted video...');
+        }
+        try {
+            this.video.play();
+        } catch(e) {
+            console.log('EXCEPT VIDEO PLAY', e);
+        }
+    };
 
-	this.pause = function() {
+    this.pause = function() {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to pause video with no data loaded.');
             return;
@@ -1186,14 +1187,14 @@ function Video(videoElement, sTrackElement) {
             return;
         }
 
-		try {
-			self.video.pause();
-		} catch(e) {
-			console.log('EXCEPT VIDEO PAUSE', e);
-		}
-	};
+        try {
+            self.video.pause();
+        } catch(e) {
+            console.log('EXCEPT VIDEO PAUSE', e);
+        }
+    };
 
-	this.setTime = function(time) {
+    this.setTime = function(time) {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to pause video with no data loaded.');
             return;
@@ -1203,10 +1204,10 @@ function Video(videoElement, sTrackElement) {
             self.seekYtVideo(time);
         }
 
-		this.video.currentTime = time;
-	};
+        this.video.currentTime = time;
+    };
 
-	this.getTime = function() {
+    this.getTime = function() {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to get video info with no data loaded.');
             return;
@@ -1216,30 +1217,30 @@ function Video(videoElement, sTrackElement) {
             return self.ytVideoCurrentTime;
         }
 
-		return this.video.currentTime;
-	};
+        return this.video.currentTime;
+    };
 
-	this.beginStream = function(socket, user) {
+    this.beginStream = function(socket, user) {
         socket.send('request_chatmessage', {
             user: user,
             message: "/stream play"
         });
-	};
+    };
 
-	this.addSubtitles = function(path, callback) {
-		return this.subtitlesHandler(path, callback);
-	};
+    this.addSubtitles = function(path, callback) {
+        return this.subtitlesHandler(path, callback);
+    };
 
-	this.removeSubtitles = function() {
-		this.subtitlesTrack.mode = "hidden";
-		this.video.textTracks[0].mode = "hidden";	
-	};
+    this.removeSubtitles = function() {
+        this.subtitlesTrack.mode = "hidden";
+        this.video.textTracks[0].mode = "hidden";
+    };
 
-	this.getVideo = function() {
-		return this.video;
-	};
+    this.getVideo = function() {
+        return this.video;
+    };
 
-	this.increaseVolume = function(val) {
+    this.increaseVolume = function(val) {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to set volume with no data loaded.');
             return;
@@ -1256,14 +1257,14 @@ function Video(videoElement, sTrackElement) {
 
 
         if (self.loadedData.kind === Cons.STREAM_KIND_YOUTUBE) {
-        	self.setYtVideoVolume(self.videoVolume * volMod);
-        	return;
+            self.setYtVideoVolume(self.videoVolume * volMod);
+            return;
         }
 
         self.video.volume = self.videoVolume;
     };
 
-	this.decreaseVolume = function(val) {
+    this.decreaseVolume = function(val) {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to set volume with no data loaded.');
             return;
@@ -1288,7 +1289,7 @@ function Video(videoElement, sTrackElement) {
         self.video.volume = self.videoVolume;
     };
 
-	this.setVolume = function(val) {
+    this.setVolume = function(val) {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to set volume with no data loaded.');
             return;
@@ -1313,7 +1314,7 @@ function Video(videoElement, sTrackElement) {
         self.video.volume = self.videoVolume;
     };
 
-	this.getDuration = function() {
+    this.getDuration = function() {
         if (!self.loadedData) {
             console.log("WARN:", 'attempt to get video duration with no data loaded.');
             return;
@@ -1323,40 +1324,40 @@ function Video(videoElement, sTrackElement) {
             return this.ytVideoInfo ? this.ytVideoInfo.duration : 0;
         }
 
-		return this.duration;
-	};
+        return this.duration;
+    };
 
-	// add event listeners
-	this.on('loadedmetadata', function() {
-		self.duration = self.video.duration;
-		self.metadataLoaded = true;
+    // add event listeners
+    this.on('loadedmetadata', function() {
+        self.duration = self.video.duration;
+        self.metadataLoaded = true;
 
         // send stream info to server
         self.emit('emitsocketdata', ['streamdata', {
-        	duration: self.duration
-		}]);
-	});
+            duration: self.duration
+        }]);
+    });
 }
 
 Video.prototype = new Emitter();
 
 function youtubeVideoIdFromUrl(url) {
-	if (url.match(/http(s)?\:\/\/youtu\.be/gi)) {
-		var segs = url.split("/");
-		return segs[segs.length - 1];
-	}
+    if (url.match(/http(s)?\:\/\/youtu\.be/gi)) {
+        var segs = url.split("/");
+        return segs[segs.length - 1];
+    }
 
-	var segs = url.split("watch?v=");
-	if (!segs.length || segs.length < 2) {
-		return segs.length ? segs[0] : url;
-	}
-	return url.split("watch?v=")[1].split("&")[0]
+    var segs = url.split("watch?v=");
+    if (!segs.length || segs.length < 2) {
+        return segs.length ? segs[0] : url;
+    }
+    return url.split("watch?v=")[1].split("&")[0]
 }
 
 function ytDurationToSeconds(ytDuration) {
-	if (!ytDuration) {
-		return 0;
-	}
+    if (!ytDuration) {
+        return 0;
+    }
 
     var match = ytDuration.match(/PT(\d+H)?(\d+M)?(\d+S)?/)
 
